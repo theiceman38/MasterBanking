64a745f146f34196923e840283c263d48dea9df3
Olivier Tavel <olivier.tavel@me.com>
2014-11-21 09:27:52 +0100

    Initial Commit

---
+++ MasterBanking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MasterBanking
+//
+//  Created by Olivier Tavel on 21/11/2014.
+//  Copyright (c) 2014 CPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MasterBanking/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MasterBanking
+//
+//  Created by Olivier Tavel on 21/11/2014.
+//  Copyright (c) 2014 CPE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
